15d7651fed3a7df1abd2ceab749c398b34235ad2
Usman Qadir <82635007+Usman-Qadir@users.noreply.github.com>
2024-01-16 00:27:33 +0500

    first commit

---
+++ my-eth-app/packages/react-app/src/components/WalletButton.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { shortenAddress, useEthers, useLookupAddress } from "@usedapp/core";
+
+import styles from "../styles";
+
+const WalletButton = () => {
+  const [rendered, setAccountAddress] = useState("");
+
+  const { ens } = useLookupAddress();
+  const { account, activateBrowserWallet, deactivate } = useEthers();
+
+  useEffect(() => {
+    if (ens) {
+      setAccountAddress(ens);
+    } else if (account) {
+      setAccountAddress(shortenAddress(account));
+    } else {
+      setAccountAddress("");
+    }
+  }, [account, ens, setAccountAddress]);
+
+  return (
+    <button
+      onClick={() => {
+        if (!account) {
+          activateBrowserWallet();
+        } else {
+          deactivate();
+        }
+      }}
+      className={styles.walletButton}
+    >
+      {rendered === "" && "Connect Wallet"}
+      {rendered !== "" && rendered}
+    </button>
+  );
+};
+
+export default WalletButton
---
+++ my-eth-app/packages/react-app/src/config.js
@@ -0,0 +1,11 @@
+import { Goerli } from "@usedapp/core";
+
+export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+
+export const DAPP_CONFIG = {
+  readOnlyChainId: Goerli.chainId,
+  readOnlyUrls: {
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/VauwGV4b7ooSV7a5JJFfeXDvKjS1kogN",
+    
+  },
+};
